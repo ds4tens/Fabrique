ff6032b642654abbaca92e2d31ca63dfa6491f99
Danil Lasuriya <d.r.lasuria@gmail.com>
2022-03-26 15:54:17 +0300

    Message filtering

--- fabrique/notifyer/urls.py
+++ fabrique/notifyer/urls.py
@@ -7,4 +7,7 @@ router.register(r'mailing', views.MailingViewSet, basename='mailingmodel')
 
 urlpatterns = [
     path('client/', views.ClientApiView.as_view(), name='client'),
+    path('message/group/', views.MessageToMailingStatView.as_view(), name='message_to_mailing'),
+    path('message/stats/', views.MessageStatView.as_view(), name='message_stat'),
+    path('message/list/', views.MessageAllView.as_view(), name='message_all'),
 ]+router.urls
--- fabrique/notifyer/views.py
+++ fabrique/notifyer/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, mixins, viewsets
-from notifyer import serializers
+from django.db.models import Count
 from notifyer import models
+from notifyer import serializers
 
 # Create your views here.
 
@@ -15,3 +16,23 @@ class MailingViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin,
 
     queryset = models.MailingModel.objects.all()
     serializer_class = serializers.MailingSerialaizer
+
+class MessageToMailingStatView(generics.ListAPIView):
+    """Информация об отправленных сообщениях опредленной рассылки"""
+    serializer_class = serializers.MessageSerializer
+
+    def get_queryset(self):
+        mailing_id = self.request.data['mailing_id']
+        return models.Message.objects.filter(mailing_id=mailing_id)
+
+class MessageStatView(generics.ListAPIView):
+    """Статистика по рассылке и статусу"""
+    serializer_class = serializers.MessageSerializer
+
+    def get_queryset(self):
+        queryset = models.Message.objects.values('mailing_id', 'status').annotate(count=Count('id'))
+        return queryset
+
+class MessageAllView(generics.ListAPIView):
+    serializer_class = serializers.MessageSerializer
+    queryset = models.Message.objects.all()
