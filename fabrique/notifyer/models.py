98c5bb4353dddaf448f2655a25d529f0a6a4cad2
Danil Lasuriya <d.r.lasuria@gmail.com>
2022-03-26 16:06:38 +0300

    models updated

--- fabrique/notifyer/models.py
+++ fabrique/notifyer/models.py
@@ -5,11 +5,10 @@ from django.db import models
 
 class Client(models.Model):
     """ Моделька получателя 'клиент' """
-    phone_number = models.IntegerField()
-    operator_code = models.SmallIntegerField()
-    # FIXME нужно будет придумать вариант реализации через выборку
-    time_zone = models.SmallIntegerField() # часовой пояс будет вычисляться вычетанием по модулю
-    tag = models.CharField(max_length=32, default='')
+    phone_number = models.IntegerField('Номер телефона')
+    operator_code = models.SmallIntegerField('Код оператора')
+    time_zone = models.CharField('Часовой пояс', max_length=128, default='Europe/Moscow')
+    tag = models.CharField('Тэг', max_length=32, default='')
 
 class MailingModel(models.Model):
     """ Моделька рассылки """
@@ -18,8 +17,8 @@ class MailingModel(models.Model):
     finish_date = models.DateTimeField('Время конца')
 
     """ Для тэга и операторского кода возможен выбор пустого поля """
-    tag = models.CharField('Тэг', max_length=32) # предполагаю текстовая метка
-    op_code = models.IntegerField(blank=True, null=True) 
+    tag = models.CharField('Тэг фильтрации', max_length=32) # предполагаю текстовая метка
+    op_code = models.IntegerField('Код оператора фильтрации', blank=True, null=True) 
 
 class Message(models.Model):
     """ 
@@ -28,6 +27,6 @@ class Message(models.Model):
     """
     mailing_id = models.ForeignKey(MailingModel, verbose_name='id рассылки', on_delete=models.DO_NOTHING)
     client_id = models.ForeignKey(Client, verbose_name='id клиента', on_delete=models.DO_NOTHING)
-    date_creation = models.DateTimeField(auto_now=True)
-    status = models.BooleanField('Статус')
+    date_creation = models.DateTimeField('Дата попытки отправки', auto_now=True)
+    status = models.BooleanField('Статус отправки')
     
