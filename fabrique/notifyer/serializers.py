30b0ae5790485fa8ba240d692f98c5f7803fe163
Danil Lasuriya <d.r.lasuria@gmail.com>
2022-03-26 15:24:24 +0300

    added mailing and message serializers

--- fabrique/notifyer/serializers.py
+++ fabrique/notifyer/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from notifyer import models
+from django.forms import ValidationError
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,4 +9,32 @@ class ClientSerializer(serializers.ModelSerializer):
             'phone_number', 'operator_code',
             'time_zone', 'tag',
         )
-    # TODO сделать валидацию
+    
+    # TODO если поле timezone будет реализовано через словарь, то нужно сделат еще проверку
+    def validate(self, attrs):
+        phone = str(attrs['phone_number'])
+        if phone[0] != '7':
+            raise ValidationError(f'{phone} номер не начинается с 7')
+
+        if len(phone) != 11:
+            raise ValidationError(f'{phone} номер должен составлять 11 символов')
+        
+        return super().validate(attrs)
+
+class MailingSerialaizer(serializers.ModelSerializer):
+    op_code = serializers.IntegerField(default=0)
+    tag = serializers.CharField(allow_blank=True)
+    class Meta:
+        model = models.MailingModel
+        fields = (
+            'content',
+            'start_date', 'finish_date',
+            'tag', 'op_code',
+        )
+    
+    # TODO validate
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Message
+        fields = "__all__"
