ae58e9f74dd17e90fcfd43989e0eba5944dd2da3
Danil Lasuriya <d.r.lasuria@gmail.com>
2022-03-26 15:30:39 +0300

    MailingViewSet added

--- fabrique/notifyer/urls.py
+++ fabrique/notifyer/urls.py
@@ -1,6 +1,10 @@
 from django.urls import  path
+from rest_framework.routers import DefaultRouter
 from notifyer import views
+
+router = DefaultRouter()
+router.register(r'mailing', views.MailingViewSet, basename='mailingmodel')
 
 urlpatterns = [
     path('client/', views.ClientApiView.as_view(), name='client'),
-]
+]+router.urls
--- fabrique/notifyer/views.py
+++ fabrique/notifyer/views.py
@@ -1,9 +1,17 @@
-from rest_framework import generics
+from rest_framework import generics, mixins, viewsets
 from notifyer import serializers
 from notifyer import models
 
 # Create your views here.
 
 class ClientApiView(generics.ListCreateAPIView):
+    # TODO переделать под сет
     queryset = models.Client.objects.all()
     serializer_class = serializers.ClientSerializer
+
+class MailingViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin, mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+
+    queryset = models.MailingModel.objects.all()
+    serializer_class = serializers.MailingSerialaizer
